c15a7e074a60c73c353c4df2e0f271258c0cf822
eliastucker11 <elias.tucker@drewcharterschools.org>
2024-09-13 12:31:18 -0400

    Domain 2
    Domain 2 Files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] locations = {"Centennial","Denver","Greeley","Lakewood"};
+            
+            var searchResult = Array.Find(locations, element => element.StartsWith("L"));
+            
+            Console.WriteLine(searchResult);
+        }
+    }  
+}
